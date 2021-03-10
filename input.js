ae91acf1a1d52a4f6c0a39a0c2ed59c6811793c7
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-10 18:37:45 +0000

    Add input handlers

--- input.js
+++ input.js
@@ -1,18 +1,27 @@
 // Setup user interface
-const setupInput = function() {
+const setupInput = function(conn) {
+  const connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', (key) => handleUserInput(key));
+  stdin.on('data', (key) => handleUserInput(key, connection));
   return stdin;
 };
 
 // User input handler
-const handleUserInput = function(key) {
+const handleUserInput = function(key, conn) {
   if (key === '\u0003') {
     console.log("Terminating game!");
     process.exit();
+  } else if (key === 'w') {
+    conn.write("Move: up");
+  } else if (key === 'a') {
+    conn.write("Move: left");
+  } else if (key === 's') {
+    conn.write("Move: down");
+  } else if (key === 'd') {
+    conn.write("Move: right");
   }
 };
 
