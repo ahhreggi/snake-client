74b17c67b58582fab9021ff390f541b3c6d5e13f
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-10 18:26:03 +0000

    Add input handler

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
+
+// Setup user interface
+const setupInput = function(cbHandler) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => cbHandler(key));
+  return stdin;
+};
+
+// User input handler
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log("Terminating game!");
+    process.exit();
+  }
+};
+
 console.log('Connecting...');
+setupInput(handleUserInput);
 connect();
