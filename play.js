35405a7429b02832c5d6bf13e0cd5c62d55e33ff
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-10 17:58:32 +0000

    Initial commit

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+// Establishes connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  // Event handler
+  conn.on('data', (data) => {
+    console.log("you ded cuz you idled");
+  });
+
+  return conn;
+}
+
+console.log('Connecting...');
+connect();
